20660928ca036e8946b7ca8d87a622446f031664
joker-jonesy <spashmcgrath94@yahoo.com>
2023-08-23 15:52:45 -0400

    kermit

---
+++ README.md
@@ -0,0 +1,15 @@
+# PuppyBowl
+
+Welcome to PuppyBowl!
+
+## Getting Started
+
+1. Live serve `index.html`. The website should be blank!
+2. Read through the comments of each function in `script.js` to get a sense of what this website should do once completed. **Do not** start writing code!
+3. Read the example test specs in `script.test.js` for `fetchAllPlayers`.
+
+## Instructions
+
+1. Complete the API functions in `script.js` so all test cases pass when you run `npm test`.
+2. Once all tests pass, complete the rest of the functions in `script.js`.
+   - The render functions should change what you see when you live serve `index.html`.
---
+++ script.js
@@ -0,0 +1,92 @@
+// Use the API_URL variable to make fetch requests to the API.
+// Replace the placeholder with your cohort name (ex: 2109-UNF-HY-WEB-PT)
+const cohortName = "YOUR COHORT NAME HERE";
+const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}`;
+
+/**
+ * Fetches all players from the API.
+ * @returns {Object[]} the array of player objects
+ */
+const fetchAllPlayers = async () => {
+  try {
+    // TODO
+  } catch (err) {
+    console.error("Uh oh, trouble fetching players!", err);
+  }
+};
+
+/**
+ * Fetches a single player from the API.
+ * @param {number} playerId
+ * @returns {Object} the player object
+ */
+const fetchSinglePlayer = async (playerId) => {
+  try {
+    // TODO
+  } catch (err) {
+    console.error(`Oh no, trouble fetching player #${playerId}!`, err);
+  }
+};
+
+/**
+ * Updates `<main>` to display a list of all players.
+ *
+ * If there are no players, a corresponding message is displayed instead.
+ *
+ * Each player is displayed in a card with the following information:
+ * - name
+ * - id
+ * - image (with alt text of the player's name)
+ *
+ * Additionally, each card has two buttons:
+ * - "See details" button that, when clicked, calls `renderSinglePlayer` to
+ *    display more information about the player
+ * - "Remove from roster" button that, when clicked, will call `removePlayer` to
+ *    remove that specific player and then re-render all players
+ *
+ * Note: this function should replace the current contents of `<main>`, not append to it.
+ * @param {Object[]} playerList - an array of player objects
+ */
+const renderAllPlayers = (playerList) => {
+  // TODO
+};
+
+/**
+ * Updates `<main>` to display a single player.
+ * The player is displayed in a card with the following information:
+ * - name
+ * - id
+ * - breed
+ * - image (with alt text of the player's name)
+ * - team name, if the player has one, or "Unassigned"
+ *
+ * The card also contains a "Back to all players" button that, when clicked,
+ * will call `renderAllPlayers` to re-render the full list of players.
+ * @param {Object} player an object representing a single player
+ */
+const renderSinglePlayer = (player) => {
+  // TODO
+};
+
+/**
+ * Initializes the app by fetching all players and rendering them to the DOM.
+ */
+const init = async () => {
+  const players = await fetchAllPlayers();
+  renderAllPlayers(players);
+
+};
+
+// This script will be run using Node when testing, so here we're doing a quick
+// check to see if we're in Node or the browser, and exporting the functions
+// we want to test if we're in Node.
+if (typeof window === "undefined") {
+  module.exports = {
+    fetchAllPlayers,
+    fetchSinglePlayer,
+    renderAllPlayers,
+    renderSinglePlayer,
+  };
+} else {
+  init();
+}
